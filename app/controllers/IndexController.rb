31d9cfc7a31035d1aeb61de5f482c3aed45bb2e7
Scott Nicol <scott@scottnicol.co.uk>
2011-10-06 18:39:17 +0100

    updated for version 0.2.2

--- app/controllers/IndexController.rb
+++ app/controllers/IndexController.rb
@@ -1,6 +1,7 @@
 class Index < Spire::MainController
   def index
-    Spire::Response.new(render("index", "haml", ["Welcome to Spire!", 
-      "This is a base app to extend your own on. Have fun!"]))
+    @title = "Spire - Light Web Framework"
+    @header = "Welcome to Spire, the lightweight web framework!"
+    render("index.haml")
   end
 end
--- config.ru
+++ config.ru
@@ -4,6 +4,8 @@ require 'spire'
 path = File.expand_path(__FILE__)
 path["config.ru"] = "app"
 
+use Rack::Static, :urls => ["/public"]
+
 run Spire::Router.new(path, {
   "default" => "index#index", # the default route!
   "/index" => "index#index",
